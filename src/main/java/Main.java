a99f4fd821d5bc7461960ef48d6e4a110b64bc3f
killioin007-source <killioin.007@gmail.com>
2026-05-06 15:31:35 -0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
+        // deal 5 cards
+        for(int i = 0; i < 5; i++) {
+            // get a card from the deck
+            Card card = deck.deal();
+            // deal that card to the hand
+            hand1.deal(card);
+        }
+        int handValue = hand1.getValue();
+        System.out.println("This hand is worth " + handValue);
+    }
+}
